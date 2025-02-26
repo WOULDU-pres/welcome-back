bf32e1df51ea1d556c0247be36c0cfc21ccc4a57
hwjoo <jhw664089@gmail.com>
2025-02-26 13:04:01 +0900

    init project

---
+++ src/main/java/me/hwjoo/welcome_back/WelcomeBackApplication.java
@@ -0,0 +1,13 @@
+package me.hwjoo.welcome_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/hwjoo/welcome_back/WelcomeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hwjoo.welcome_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelcomeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
